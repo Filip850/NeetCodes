9e8a35e3cb07bb0e81647d217b210d26f60bd2b5
Filip850 <filski850@gmail.com>
2026-03-16 21:49:58 +0100

    refactor(LongestConsecutive): Static imports and protected methods

--- src/test/java/ArraysHashing/LongestConsecutiveTest.java
+++ src/test/java/ArraysHashing/LongestConsecutiveTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pl.filip850.ArraysHashing.LongestConsecutive;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LongestConsecutiveTest {
 
   private final LongestConsecutive longestConsecutive;
@@ -15,14 +17,14 @@ public class LongestConsecutiveTest {
 
   @Test
   @DisplayName("Check is longest consecutive calculated correctly - Case 1")
-  public void longestConsecutive_case1(){
-    Assertions.assertEquals(4, longestConsecutive.longestConsecutive(new int[] {2,20,4,10,3,4,5}));
+  void longestConsecutive_case1(){
+    assertEquals(4, longestConsecutive.longestConsecutive(new int[] {2,20,4,10,3,4,5}));
   }
 
   @Test
   @DisplayName("Check is longest consecutive calculated correctly - Case 2")
-  public void longestConsecutive_case2(){
-    Assertions.assertEquals(7, longestConsecutive.longestConsecutive(new int[] {0,3,2,5,4,6,1,1}));
+  void longestConsecutive_case2(){
+    assertEquals(7, longestConsecutive.longestConsecutive(new int[] {0,3,2,5,4,6,1,1}));
 
   }
 }
